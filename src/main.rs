6e3ea3a32fdd7f5a39a92aa907eb6502dc388f18
piotmag769 <piotrmagiera150@gmail.com>
2025-09-30 16:44:15 +0200

    Scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use dap::errors::ServerError;
+
+use cairo_debugger::CairoDebugger;
+
+// TODO: there will be no bin target in the future.
+fn main() -> Result<(), ServerError> {
+    CairoDebugger::default().run()
+}
