6770d292ba9dfa08317afcae3f9014b4ff8845ac
piotmag769 <piotrmagiera150@gmail.com>
2025-10-07 16:15:58 +0200

    Setup CI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use dap::errors::ServerError;
-
 use cairo_debugger::CairoDebugger;
+use dap::errors::ServerError;
 
 // TODO: there will be no bin target in the future.
 fn main() -> Result<(), ServerError> {
