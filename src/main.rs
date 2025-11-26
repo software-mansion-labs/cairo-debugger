160a31f901498b35c68e89aea808ac299e356e27
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2025-11-26 12:41:35 +0100

    Remove main.rs

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use cairo_debugger::CairoDebugger;
-use dap::errors::ServerError;
-
-// TODO: there will be no bin target in the future.
-fn main() -> Result<(), ServerError> {
-    CairoDebugger::connect()?.run()
-}
