0a954c9c84f8fefe734ed0dda7348734745a423a
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2025-11-27 15:15:23 +0100

    Handle all requests inside `handle_request`

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,5 +1,4 @@
-use dap::events::Event;
-use dap::prelude::ResponseBody;
+use anyhow::Result;
 
 use crate::connection::Connection;
 
@@ -9,30 +8,15 @@ pub struct CairoDebugger {
     connection: Connection,
 }
 
-enum ServerResponse {
-    Success(ResponseBody),
-    Error(String),
-    Event(Event),
-    SuccessThenEvent(ResponseBody, Event),
-}
-
 impl CairoDebugger {
-    pub fn connect() -> anyhow::Result<Self> {
+    pub fn connect() -> Result<Self> {
         let connection = Connection::new()?;
         Ok(Self { connection })
     }
 
-    pub fn run(&self) -> anyhow::Result<()> {
+    pub fn run(&self) -> Result<()> {
         while let Ok(req) = self.connection.next_request() {
-            match self.handle_request(&req) {
-                ServerResponse::Success(body) => self.connection.send_success(req, body)?,
-                ServerResponse::Error(msg) => self.connection.send_error(req, &msg)?,
-                ServerResponse::Event(event) => self.connection.send_event(event)?,
-                ServerResponse::SuccessThenEvent(body, event) => {
-                    self.connection.send_success(req, body)?;
-                    self.connection.send_event(event)?;
-                }
-            }
+            self.handle_request(req)?;
         }
 
         Ok(())
--- src/debugger/handler.rs
+++ src/debugger/handler.rs
@@ -1,3 +1,4 @@
+use anyhow::{Result, bail};
 use dap::events::{Event, StoppedEventBody};
 use dap::prelude::{Command, Request, ResponseBody};
 use dap::responses::{
@@ -6,20 +7,15 @@ use dap::responses::{
 };
 use dap::types::{Breakpoint, Capabilities, Source, StackFrame, StoppedEventReason, Thread};
 use tracing::trace;
+
 use crate::CairoDebugger;
-use crate::debugger::ServerResponse;
 
 pub enum HandleResult {
     Handled,
-    Trigger(NextAction),
-}
-
-pub enum NextAction {
-    FinishInit,
 }
 
 impl CairoDebugger {
-    pub(crate) fn handle_request(&self, request: &Request) -> ServerResponse {
+    pub(crate) fn handle_request(&self, request: Request) -> Result<HandleResult> {
         match &request.command {
             // We have not yet decided if we want to support these.
             Command::BreakpointLocations(_)
@@ -47,55 +43,75 @@ impl CairoDebugger {
             | Command::WriteMemory(_) => {
                 // If we receive these with current capabilities, it is the client's fault.
                 let msg = format!("Received an unsupported request: {request:?}");
-                ServerResponse::Error(msg)
+                self.connection.send_error(request, &msg)?;
+                bail!("Unsupported request");
             }
 
             // These may be supported after the MVP.
             // Nonetheless, if we receive these with current capabilities,
             // it is the client's fault.
             Command::ReverseContinue(_) => {
-                ServerResponse::Error("Step back is not yet supported".into())
+                self.connection.send_error(request, "Reverse continue is not yet supported")?;
+                bail!("Reverse continue is not yet supported");
             }
-            Command::StepBack(_) => ServerResponse::Error("Step back is not yet supported".into()),
+            Command::StepBack(_) => {
+                self.connection.send_error(request, "Step back is not yet supported")?;
+                bail!("Step back is not yet supported");
+            }
             Command::SetFunctionBreakpoints(_) => {
-                ServerResponse::Error("Set function breakpoints is not yet supported".into())
+                self.connection
+                    .send_error(request, "Set function breakpoints is not yet supported")?;
+                bail!("Set function breakpoints is not yet supported");
             }
 
             // It makes no sense to send `attach` in the current architecture.
-            Command::Attach(_) => ServerResponse::Error("Attach is not supported".into()),
+            Command::Attach(_) => {
+                self.connection.send_error(request, "Attach is not supported")?;
+                bail!("Unsupported request");
+            }
 
             // Supported requests.
             Command::Initialize(args) => {
                 trace!("Initialized a client: {:?}", args.client_name);
-
-                ServerResponse::Success(ResponseBody::Initialize(Capabilities {
-                    supports_configuration_done_request: Some(true),
-                    ..Default::default()
-                }))
+                self.connection.send_success(
+                    request,
+                    ResponseBody::Initialize(Capabilities {
+                        supports_configuration_done_request: Some(true),
+                        ..Default::default()
+                    }),
+                )?;
+                self.connection.send_event(Event::Initialized)?;
+                Ok(HandleResult::Handled)
             }
             Command::ConfigurationDone => {
                 trace!("Configuration done");
-                ServerResponse::Success(ResponseBody::ConfigurationDone)
+                self.connection.send_success(request, ResponseBody::ConfigurationDone)?;
+                Ok(HandleResult::Handled)
             }
             Command::Continue(_) => {
                 todo!()
             }
             Command::Launch(_) => {
                 // Start running the Cairo program here.
-                ServerResponse::SuccessThenEvent(ResponseBody::Launch, Event::Initialized)
+                self.connection.send_success(request, ResponseBody::Launch)?;
+                Ok(HandleResult::Handled)
             }
             Command::Next(_) => {
                 todo!()
             }
-            Command::Pause(_) => ServerResponse::Event(Event::Stopped(StoppedEventBody {
-                reason: StoppedEventReason::Pause,
-                thread_id: Some(0),
-                description: None,
-                preserve_focus_hint: None,
-                text: None,
-                all_threads_stopped: Some(true),
-                hit_breakpoint_ids: None,
-            })),
+            Command::Pause(_) => {
+                self.connection.send_event(Event::Stopped(StoppedEventBody {
+                    reason: StoppedEventReason::Pause,
+                    thread_id: Some(0),
+                    description: None,
+                    preserve_focus_hint: None,
+                    text: None,
+                    all_threads_stopped: Some(true),
+                    hit_breakpoint_ids: None,
+                }))?;
+                self.connection.send_success(request, ResponseBody::Pause)?;
+                Ok(HandleResult::Handled)
+            }
             Command::SetBreakpoints(args) => {
                 let mut response_bps = Vec::new();
                 if let Some(requested_bps) = &args.breakpoints {
@@ -109,27 +125,35 @@ impl CairoDebugger {
                         });
                     }
                 }
-                ServerResponse::Success(ResponseBody::SetBreakpoints(SetBreakpointsResponse {
-                    breakpoints: response_bps,
-                }))
+                self.connection.send_success(
+                    request,
+                    ResponseBody::SetBreakpoints(SetBreakpointsResponse {
+                        breakpoints: response_bps,
+                    }),
+                )?;
+                Ok(HandleResult::Handled)
             }
             Command::Source(_) => {
                 todo!()
             }
             Command::StackTrace(_) => {
-                ServerResponse::Success(ResponseBody::StackTrace(StackTraceResponse {
-                    stack_frames: vec![StackFrame {
-                        id: 1,
-                        name: "test".to_string(),
-                        // Replace it with the actual source path.
-                        // Otherwise, the debugger will crush after returning this response.
-                        source: Some(Source { name: None, path: None, ..Default::default() }),
-                        line: 1,
-                        column: 1,
-                        ..Default::default()
-                    }],
-                    total_frames: Some(1),
-                }))
+                self.connection.send_success(
+                    request,
+                    ResponseBody::StackTrace(StackTraceResponse {
+                        stack_frames: vec![StackFrame {
+                            id: 1,
+                            name: "test".to_string(),
+                            // Replace it with the actual source path.
+                            // Otherwise, the debugger will crush after returning this response.
+                            source: Some(Source { name: None, path: None, ..Default::default() }),
+                            line: 1,
+                            column: 1,
+                            ..Default::default()
+                        }],
+                        total_frames: Some(1),
+                    }),
+                )?;
+                Ok(HandleResult::Handled)
             }
             Command::StepIn(_) => {
                 todo!()
@@ -139,32 +163,49 @@ impl CairoDebugger {
             }
 
             Command::Evaluate(_) => {
-                ServerResponse::Success(ResponseBody::Evaluate(EvaluateResponse {
-                    // Return whatever since we cannot opt out of supporting this request.
-                    result: "".to_string(),
-                    type_field: None,
-                    presentation_hint: None,
-                    variables_reference: 0,
-                    named_variables: None,
-                    indexed_variables: None,
-                    memory_reference: None,
-                }))
+                self.connection.send_success(
+                    request,
+                    ResponseBody::Evaluate(EvaluateResponse {
+                        // Return whatever since we cannot opt out of supporting this request.
+                        result: "".to_string(),
+                        type_field: None,
+                        presentation_hint: None,
+                        variables_reference: 0,
+                        named_variables: None,
+                        indexed_variables: None,
+                        memory_reference: None,
+                    }),
+                )?;
+                Ok(HandleResult::Handled)
             }
             Command::Threads => {
-                ServerResponse::Success(ResponseBody::Threads(ThreadsResponse {
-                    // Return a single thread.
-                    threads: vec![Thread { id: 0, name: "".to_string() }],
-                }))
+                self.connection.send_success(
+                    request,
+                    ResponseBody::Threads(ThreadsResponse {
+                        // Return a single thread.
+                        threads: vec![Thread { id: 0, name: "".to_string() }],
+                    }),
+                )?;
+                Ok(HandleResult::Handled)
             }
             Command::Variables(_) => {
-                ServerResponse::Success(ResponseBody::Variables(VariablesResponse {
-                    // Return no variables.
-                    variables: vec![],
-                }))
+                self.connection.send_success(
+                    request,
+                    ResponseBody::Variables(VariablesResponse {
+                        // Return no variables.
+                        variables: vec![],
+                    }),
+                )?;
+                Ok(HandleResult::Handled)
             }
             Command::Scopes(_) => {
                 // Return no scopes.
-                ServerResponse::Success(ResponseBody::Scopes(ScopesResponse { scopes: vec![] }))
+                // Return no scopes.
+                self.connection.send_success(
+                    request,
+                    ResponseBody::Scopes(ScopesResponse { scopes: vec![] }),
+                )?;
+                Ok(HandleResult::Handled)
             }
         }
     }
