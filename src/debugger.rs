85b3ff3304e1e64da7ebab095058b9120f59de6b
ddoktorski <45050160+ddoktorski@users.noreply.github.com>
2025-12-04 19:14:55 +0100

    Add initialization flow (#30)
    * Add initialization flow
    
    * Add init debug trace
    
    * Link todo

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
+use tracing::debug;
 
 use crate::connection::Connection;
+use crate::debugger::handler::{HandleResult, NextAction};
 
 mod handler;
 
@@ -9,9 +11,25 @@ pub struct CairoDebugger {
 }
 
 impl CairoDebugger {
-    pub fn connect() -> Result<Self> {
+    pub fn connect_and_initialize() -> Result<Self> {
         let connection = Connection::new()?;
-        Ok(Self { connection })
+        let debugger = Self { connection };
+        debugger.initialize()?;
+
+        Ok(debugger)
+    }
+
+    fn initialize(&self) -> Result<()> {
+        loop {
+            // TODO(#35)
+            let request = self.connection.next_request()?;
+            if let HandleResult::Trigger(NextAction::FinishInit) = self.handle_request(request)? {
+                debug!("Initialization finished");
+                break;
+            }
+        }
+
+        Ok(())
     }
 
     pub fn run(&self) -> Result<()> {
