8536aef2cdb55e90417e92f651971037409079e7
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2025-12-16 10:36:29 +0100

    Return error if debug_info is empty

--- src/debugger/context.rs
+++ src/debugger/context.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use cairo_annotations::annotations::TryFromDebugInfo;
 use cairo_annotations::annotations::coverage::CoverageAnnotationsV1 as SierraCodeLocations;
 use cairo_lang_sierra::program::ProgramArtifact;
@@ -14,9 +14,10 @@ impl Context {
     pub fn new(sierra_path: &Utf8Path) -> Result<Self> {
         let content = fs::read_to_string(sierra_path).expect("Failed to load sierra file");
         let sierra_program: ProgramArtifact = serde_json::from_str(&content)?;
-        let code_locations = SierraCodeLocations::try_from_debug_info(
-            &sierra_program.debug_info.expect("debug_info must be present in compiled sierra"),
-        )?;
+        let debug_info = sierra_program
+            .debug_info
+            .ok_or_else(|| anyhow!("debug_info must be present in compiled sierra"))?;
+        let code_locations = SierraCodeLocations::try_from_debug_info(&debug_info)?;
 
         Ok(Self { code_locations })
     }
