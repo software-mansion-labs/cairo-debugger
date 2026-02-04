d91186f08c622440801c0fe66c7b1e6d868e914f
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2026-02-04 13:31:21 +0100

    Stop on the same breakpoint iff ui state changed (#70)
    * Skip state update when not on program segment
    
    commit-id:fb4953ec
    
    * Stop on the same breakpoint iff ui state changed
    
    commit-id:00268697

--- src/debugger/state.rs
+++ src/debugger/state.rs
@@ -1,8 +1,11 @@
+use std::cmp::PartialEq;
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
 
+use cairo_annotations::annotations::coverage::CodeLocation;
 use cairo_lang_sierra::program::StatementIdx;
 use cairo_vm::vm::vm_core::VirtualMachine;
+use dap::types::StackFrame;
 use tracing::{debug, trace};
 
 use crate::debugger::call_stack::CallStack;
@@ -13,9 +16,10 @@ type SourcePath = String;
 pub struct State {
     configuration_done: bool,
     execution_stopped: bool,
-    pub breakpoints: HashMap<SourcePath, HashSet<StatementIdx>>,
+    breakpoints: HashMap<SourcePath, HashSet<StatementIdx>>,
     pub current_statement_idx: StatementIdx,
     pub call_stack: CallStack,
+    last_breakpoint_hit: Option<BreakpointHit>,
 }
 
 impl State {
@@ -26,6 +30,7 @@ impl State {
             breakpoints: HashMap::default(),
             current_statement_idx: StatementIdx(0),
             call_stack: CallStack::default(),
+            last_breakpoint_hit: None,
         }
     }
 
@@ -40,7 +45,7 @@ impl State {
         }
 
         self.current_statement_idx = ctx.statement_idx_for_pc(current_pc.offset);
-        self.call_stack.update(self.current_statement_idx, ctx)
+        self.call_stack.update(self.current_statement_idx, ctx);
     }
 
     pub fn is_configuration_done(&self) -> bool {
@@ -89,5 +94,52 @@ impl State {
 
     pub fn clear_breakpoints(&mut self, source: &SourcePath) {
         self.breakpoints.remove(source);
+    }
+
+    pub fn was_breakpoint_hit(&mut self, ctx: &Context) -> bool {
+        if self
+            .breakpoints
+            .values()
+            .flatten()
+            .all(|statement_idx| *statement_idx != self.current_statement_idx)
+        {
+            return false;
+        }
+
+        let location = ctx
+            .code_location_for_statement_idx(self.current_statement_idx)
+            .expect("Breakpoint statement was expected to have corresponding code location");
+        let ui_state = UiState::build(self, ctx);
+        let breakpoint_hit = Some(BreakpointHit { location, ui_state });
+
+        // If we hit the same breakpoint and the ui state is the same,
+        // there is nothing new to display to the user - so we don't stop there.
+        if self.last_breakpoint_hit == breakpoint_hit {
+            return false;
+        }
+
+        self.last_breakpoint_hit = breakpoint_hit;
+        true
+    }
+}
+
+#[derive(PartialEq)]
+struct BreakpointHit {
+    location: CodeLocation,
+    /// State of the UI at the time when the breakpoint was hit.
+    ui_state: UiState,
+}
+
+/// Represents the state of the debugger from the user's point of view.
+/// E.g. `stack_trace` is visible to a user through [`dap::prelude::Command::StackTrace`] request.
+#[derive(PartialEq)]
+struct UiState {
+    stack_trace: Vec<StackFrame>,
+}
+
+impl UiState {
+    fn build(state: &State, ctx: &Context) -> Self {
+        let stack_trace = state.call_stack.get_frames(state.current_statement_idx, ctx);
+        UiState { stack_trace }
     }
 }
